9364b9265a30da1f3620a28bc9d2369dfdabcc0a
Brunu <comachiobelobruno@protonmail.com>
2022-11-17 20:33:25 -0400

    Versão base

--- AFD.cpp
+++ AFD.cpp
@@ -1,4 +1,20 @@
-//oi
-// oi lindo
-// não concordo
-// MALUQUINHO
+/*
+Trabalho da materia de TLFAC 2022/2 criando um simulador de
+Automatos Finitos Deterministicos
+
+Alunos:
+Bruno Belo
+Matheus Parizotto
+Lucca Souza
+Lucas Freitas
+*/
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+    return 0;
+}
